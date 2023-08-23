4b59f7dd0abf9c348772bca6af40d7bfe247eab3
indiamaris <indiamaris@gmail.com>
2023-08-23 16:14:56 +0200

    build navbar

--- src/GAME-SRC/AppGame.tsx
+++ src/GAME-SRC/AppGame.tsx
@@ -1,6 +1,7 @@
 /** @format */
 
 import { Grid, GridItem, Show } from '@chakra-ui/react';
+import NavBar from './components/navBar';
 
 const AppGame = () => {
 	return (
@@ -9,17 +10,13 @@ const AppGame = () => {
 				base: `' nav' 'main'`,
 				lg: `'nav nav' 'aside main'`,
 			}}>
-			<GridItem
-				area='nav'
-				bg='pink'>
-				{' '}
-				NAv
+			<GridItem area='nav'>
+				<NavBar />
 			</GridItem>
 			<Show above='lg'>
 				<GridItem
 					area='aside'
 					bg='gold'>
-					{' '}
 					aside
 				</GridItem>
 			</Show>
---
+++ src/GAME-SRC/components/navBar.jsx
@@ -0,0 +1,16 @@
+/** @format */
+
+import { HStack, Image, Text } from '@chakra-ui/react';
+import logo from '../assets/Logo/logo.webp';
+/** @format */
+const NavBar = () => {
+	return (
+		<HStack>
+			<Image src={logo} boxSize={'60px'} />
+			<Text>NavBar</Text>
+		</HStack>
+	);
+};
+
+export default NavBar;
+
